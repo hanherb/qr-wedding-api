30041fd0ae074512deaa9e7de5c0e21d277b0f65
hanindyo <hanindyo@femaledaily.com>
2023-08-27 13:35:36 +0700

    fix: add invitation data

--- src/data/invitation.ts
+++ src/data/invitation.ts
@@ -1,8 +1,62 @@
-export const rawInvitations =[
-    {
-      "name": "Hanindyo Herbowo"
-    },
-    {
-      "name": "Niken Monica Desiyanti"
-    }
+export const rawInvitations = [
+  {
+    "name": "Saptadji"
+  },
+  {
+    "name": "Agung & Otty"
+  },
+  {
+    "name": "Rian & Dhini"
+  },
+  {
+    "name": "Ricar"
+  },
+  {
+    "name": "Berry"
+  },
+  {
+    "name": "Ami"
+  },
+  {
+    "name": "Ramsi"
+  },
+  {
+    "name": "Chandra"
+  },
+  {
+    "name": "Ghinta"
+  },
+  {
+    "name": "Ewi"
+  },
+  {
+    "name": "Yanthi Baramuli"
+  },
+  {
+    "name": "Yetti"
+  },
+  {
+    "name": "Yora"
+  },
+  {
+    "name": "Elin"
+  },
+  {
+    "name": "Ajeng"
+  },
+  {
+    "name": "Eli"
+  },
+  {
+    "name": "Sulis"
+  },
+  {
+    "name": "Anggun & Alfa"
+  },
+  {
+    "name": "Rora & Dedi"
+  },
+  {
+    "name": "Anggi & Amir"
+  }
 ]
